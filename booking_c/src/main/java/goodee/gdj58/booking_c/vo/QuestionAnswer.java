aad8ad62f1470fe1dbf6528f8d5ae8006f8081e2
Ga5ri <jss6868@naver.com>
2023-03-03 14:39:47 +0900

    [장준영] VO 추가

--- booking_c/src/main/java/goodee/gdj58/booking_c/vo/QuestionAnswer.java
+++ booking_c/src/main/java/goodee/gdj58/booking_c/vo/QuestionAnswer.java
@@ -3,8 +3,8 @@ package goodee.gdj58.booking_c.vo;
 import lombok.Data;
 
 @Data
-public class Report {
-	private int reportNo;
-	private String reportMemo;
+public class QuestionAnswer {
+	private int questionNo;
+	private String questionComment;
 	private String createdate;
 }
